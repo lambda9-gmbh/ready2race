3067d79e495ce44d0b83431b5ce7c491b1c70ae6
Mario Köhler <mko@lambda9.de>
2025-01-30 17:50:53 +0100

    Clean code

--- backend/src/main/kotlin/de/lambda9/ready2race/backend/pagination/Extensions.kt
+++ backend/src/main/kotlin/de/lambda9/ready2race/backend/pagination/Extensions.kt
@@ -3,9 +3,11 @@ package de.lambda9.ready2race.backend.pagination
 import org.jooq.SortField
 
 fun <S: Sortable> List<Order<S>>.toOrderBy(): List<SortField<*>> =
-    this.map { it.direction to it.field.toField() }.map { (r, f) ->
-        when (r) {
-            Direction.DESC -> f.desc()
-            Direction.ASC -> f.asc()
+    this.map { order ->
+        order.field.toField().let {
+            when (order.direction) {
+                Direction.DESC -> it.desc()
+                Direction.ASC -> it.asc()
+            }
         }
     }
