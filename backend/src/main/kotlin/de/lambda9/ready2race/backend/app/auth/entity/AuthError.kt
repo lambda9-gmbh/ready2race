38267d956c79c63a30cb60aa50379e7f2e17951f
Mario Köhler <mko@lambda9.de>
2025-01-30 17:48:02 +0100

    Extract AuthError from AuthService and use more fitting 403 - Forbidden status code for missing privilege error

---
+++ backend/src/main/kotlin/de/lambda9/ready2race/backend/app/auth/entity/AuthError.kt
@@ -0,0 +1,18 @@
+package de.lambda9.ready2race.backend.app.auth.entity
+
+import de.lambda9.ready2race.backend.app.ServiceError
+import de.lambda9.ready2race.backend.responses.ApiError
+import io.ktor.http.*
+
+enum class AuthError: ServiceError {
+
+    CredentialsIncorrect,
+    TokenInvalid,
+    PrivilegeMissing;
+
+    override fun respond(): ApiError = when (this) {
+        CredentialsIncorrect -> ApiError(status = HttpStatusCode.Unauthorized, message = "Incorrect credentials")
+        TokenInvalid -> ApiError(status = HttpStatusCode.Unauthorized, message = "Invalid session token")
+        PrivilegeMissing -> ApiError(status = HttpStatusCode.Forbidden, message = "Missing privilege")
+    }
+}
