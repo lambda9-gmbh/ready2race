b0f067b6d6878f9afc2aefa7bd2a238998da6373
Benjamin Byl <bby@lambda9.de>
2025-07-18 13:34:28 +0200

    add namedParticipant Requirments for Events {fix cr}

--- frontend/src/api/sdk.gen.ts
+++ frontend/src/api/sdk.gen.ts
@@ -188,9 +188,6 @@ import type {
     DeleteNamedParticipantData,
     DeleteNamedParticipantError,
     DeleteNamedParticipantResponse,
-    GetNamedParticipantRequirementsForEventData,
-    GetNamedParticipantRequirementsForEventError,
-    GetNamedParticipantRequirementsForEventResponse,
     GetNamedParticipantsForEventData,
     GetNamedParticipantsForEventError,
     GetNamedParticipantsForEventResponse,
@@ -1177,19 +1174,6 @@ export const deleteNamedParticipant = <ThrowOnError extends boolean = false>(
     >({
         ...options,
         url: '/namedParticipant/{namedParticipantId}',
-    })
-}
-
-export const getNamedParticipantRequirementsForEvent = <ThrowOnError extends boolean = false>(
-    options: OptionsLegacyParser<GetNamedParticipantRequirementsForEventData, ThrowOnError>,
-) => {
-    return (options?.client ?? client).get<
-        GetNamedParticipantRequirementsForEventResponse,
-        GetNamedParticipantRequirementsForEventError,
-        ThrowOnError
-    >({
-        ...options,
-        url: '/namedParticipant/{namedParticipantId}/event/{eventId}/requirements',
     })
 }
 
