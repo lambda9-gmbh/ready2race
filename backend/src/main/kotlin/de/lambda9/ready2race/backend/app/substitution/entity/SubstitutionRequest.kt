025088b59b3a2870042fc083666d5a3c9cbe1289
tschwarz <tsc@lambda9.de>
2025-07-22 15:53:43 +0200

    Add substitution request validation

--- backend/src/main/kotlin/de/lambda9/ready2race/backend/app/substitution/entity/SubstitutionRequest.kt
+++ backend/src/main/kotlin/de/lambda9/ready2race/backend/app/substitution/entity/SubstitutionRequest.kt
@@ -2,16 +2,16 @@ package de.lambda9.ready2race.backend.app.substitution.entity
 
 import de.lambda9.ready2race.backend.validation.Validatable
 import de.lambda9.ready2race.backend.validation.ValidationResult
+import de.lambda9.ready2race.backend.validation.validate
+import de.lambda9.ready2race.backend.validation.validators.StringValidators.notBlank
 import java.util.UUID
 
 data class SubstitutionRequest(
     val participantOut: UUID,
     val participantIn: UUID,
     val reason: String?,
-    //val participantRequirements: List<SubstitutionHasParticipantRequirementRequest>,
-    //val swappedParticipantRequirements: List<SubstitutionHasParticipantRequirementRequest>?
 ) : Validatable {
-    override fun validate(): ValidationResult = ValidationResult.Valid // todo: validate
+    override fun validate(): ValidationResult = this::reason validate notBlank
 
     companion object {
         val example
@@ -19,8 +19,6 @@ data class SubstitutionRequest(
                 participantOut = UUID.randomUUID(),
                 participantIn = UUID.randomUUID(),
                 reason = "Reason for the substitution",
-                //participantRequirements = listOf(SubstitutionHasParticipantRequirementRequest.example),
-                //swappedParticipantRequirements = listOf(SubstitutionHasParticipantRequirementRequest.example)
             )
     }
 }
